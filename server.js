72ea24afc7c4bd47ad791cb32aa20ab6a21292dc
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2021-01-26 12:05:14 -0500

    Added mongoose to express server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // 1. Require Express
 const express = require("express");
+const mongoose = require("mongoose");
 
 // 2. Create instance of Express
 const app = express();
@@ -11,6 +12,25 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mern-portfolio",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected!");
+});
+
+connection.on("error", (err) => {
+  console.log(`Mongoose connection error: ${err}`);
+});
 // 6. Create Routes
 
 app.get("/api/config", (req, res) => {
